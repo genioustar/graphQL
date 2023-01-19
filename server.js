139d4a2a268b145f4f1613ab8ec8fd54fe62f9e5
genioustar <4574gkdl>
2023-01-19 17:18:52 +0900

    그래프큐엘 마무리!

--- server.js
+++ server.js
@@ -1,25 +1,45 @@
 import { ApolloServer, gql } from "apollo-server";
-
+import fetch from "node-fetch";
 /** ApolloServer 시작을 위해서 파라미터로 변수명을 무조건!!! typeDefs로 해줘야함!
  * gql의 SDL을 작성시 type Query는 필수 항목!
  * Query 안의 text라는 값은 REST API의 GET /text와 같은 의미!
  */
 
-const tweets = [
+let tweets = [
   {
     id: "1",
     text: "first one!",
+    userId: "2",
   },
   {
     id: "2",
     text: "second one",
+    userId: "1",
+  },
+];
+
+let users = [
+  {
+    id: "1",
+    firstName: "nico",
+    lastName: "las",
+  },
+  {
+    id: "2",
+    firstName: "Elon",
+    lastName: "Mask",
   },
 ];
 
 const typeDefs = gql`
   type User {
     id: ID!
-    username: String!
+    firstName: String!
+    lastName: String!
+    """
+    full name is firstName + lastName
+    """
+    fullName: String!
   }
   type Tweet {
     id: ID!
@@ -27,12 +47,38 @@ const typeDefs = gql`
     author: User!
   }
   type Query {
+    allUsers: [User!]!
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
+    allMovies: [Movie!]!
+    movie(id: ID!): Movie
   }
   type Mutation {
     postTweet(text: String!, userId: ID!): Tweet!
     deleteTweet(id: ID!): Boolean!
+  }
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+    title_english: String!
+    title_long: String!
+    slug: String!
+    year: Int!
+    rating: Float!
+    runtime: Float!
+    genres: [String]!
+    summary: String
+    description_full: String!
+    synopsis: String
+    yt_trailer_code: String!
+    language: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_image: String!
+    large_cover_image: String!
   }
 `; //여기에 graphql의 schema definition language(SDL)를 설정한다.
 
@@ -44,6 +90,48 @@ const resolvers = {
     },
     allTweets() {
       return tweets;
+    },
+    allUsers() {
+      console.log("allUsers called");
+      return users;
+    },
+    allMovies() {
+      return fetch("https://yts.mx/api/v2/list_movies.json")
+        .then((r) => r.json())
+        .then((json) => json.data.movies);
+    },
+    movie(_, { id }) {
+      return fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        .then((r) => r.json())
+        .then((json) => json.data.movie);
+    },
+  },
+  Mutation: {
+    postTweet(_, { text, userId }) {
+      const newTweet = {
+        id: tweets.length + 1,
+        text,
+        userId,
+      };
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(_, { id }) {
+      const tweet = tweets.find((tweet) => tweet.id === id);
+      if (!tweet) return false;
+      tweets = tweets.filter((tweet) => tweet.id !== id);
+      return true;
+    },
+  },
+  User: {
+    fullName({ firstName, lastName }) {
+      console.log("fullName called");
+      return `${firstName} ${lastName}`;
+    },
+  },
+  Tweet: {
+    author({ userId }) {
+      return users.find((user) => userId === user.id);
     },
   },
 };
